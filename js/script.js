686cb513e5b8850b6603137bd5b386c3413628f5
Egil Hansen <egil@assimilated.dk>
2012-05-25 17:18:48 +0200

    Timer, styles, counters, done

--- js/script.js
+++ js/script.js
@@ -1,103 +1,114 @@
 /* Author: Egil Hansen */
-(function($) {
+(function ($) {
 
-  var runToggler = $('#run-toggler'),
-      work = $('#work'),
-      workLabel = $('label[for="work"] > span'),
-      breakTime = $('#break'),
-      breakLabel = $('label[for="break"] > span'),
-      rest = $('#rest'),
-      restLabel = $('label[for="rest"] > span'),
-      timer = $('#timer'),
-      stateElm = $('#state'),
-      pomoCountElm = $('pomodoro-count'),
-      state = {},
-      countdown,
-      updateState;
+    var timerElm = $('#timer'),
+        stateElm = $('#state .value'),
+        pomoCountElm = $('#pomodoro-count .value'),
+        state = {},
+        countdown,
+        updateState,
+        setTarget,
+        toogleTimer;
 
-  state = {
-    time: 0,
-    target: 0,
-    type: 0,
-    pomocount: 0,
-    timeoutId,
-  };
+    state = {
+        time: 0,
+        target: 0,
+        type: '',
+        pomocount: 0,
+        timeoutId: undefined
+    };
 
-  countdown = function() {
-    var mins, secs;
-    state.timeoutId = window.setTimeout(countdown, 1000);
-    state.time += 1;
-    mins = Math.floor(state.time / 60);
-    secs = state.time % 60;
+    setTarget = function () {
+        state.target = $('#' + state.type).val() * 60;
+    };
 
-    // prettify for
-    mins = mins < 10 ? "0" + mins : mins;
-    secs = secs < 10 ? "0" + secs : secs;
-    timer.text(mins + ":" + secs);
-    if(time > target) {
-      state.time = 0;
-      updateState();
-    }
-  };
+    // (re)start the counter and update the display
+    countdown = function () {
+        var mins, secs;
+        // restart timer and update time
+        state.timeoutId = window.setTimeout(countdown, 1000);
+        state.time += 1;
 
-  updateState = function() {
-    if(workCount === 0) {
-        // rest
-        countdownType = 2;
-        target = rest.val() * 60;
-        $('body').css('background-color', 'red').effect("pulsate", { times: 4 }, 1000);
-      } else if(workCount > 0) {
-        // break
-        countdownType = 1;
-        target = breakTime.val() * 60;
-        $('body').css('background-color', 'yellow').effect("pulsate", { times: 4 }, 1000);
-      } else {
-        countdownType = 1;
-        target = work.val() * 60;
-        $('body').css('background-color', 'white');
-      }
+        mins = Math.floor(state.time / 60);
+        secs = state.time % 60;
 
-  };
+        // update rest of UI if needed
+        if (state.time > state.target) {
+            state.time = 0;
+            updateState();
+        } else {
+            // prettify for output
+            mins = mins < 10 ? "0" + mins : mins;
+            secs = secs < 10 ? "0" + secs : secs;
+            timerElm.text(mins + ":" + secs);
+        }
+    };
 
-  $(document).ready(function() {
+    // update the interface
+    updateState = function () {
+        // what kind of timer shoud we use now?
+        if (state.type === 'work') {
+            if (state.pomocount < 4) {
+                // increment pomodoro counter
+                state.type = 'break';
+                stateElm.text("Break");
+                $('h1, #state, #timer, #pomodoro-count')
+                    .toggleClass('break')
+                    .effect("pulsate", { times: 4 }, 500);
+            } else {
+                state.pomocount = 0;
+                state.type = 'rest';
+                stateElm.text("Rest");
+                $('h1, #state, #timer, #pomodoro-count')
+                    .toggleClass('rest')
+                    .effect("pulsate", { times: 4 }, 500);
+            }
+        } else {
+            state.pomocount += 1;
+            pomoCountElm.text(state.pomocount);
+            state.type = 'work';
+            stateElm.text("Working");
+            $('h1, #state, #timer, #pomodoro-count').removeClass();
+        }
 
-    work.on('change', function(evt) {
-      if(countdownType === 0) {
-        target = work.val() * 60;
-      }
+        // update target time
+        setTarget();
+    };
+
+    toogleTimer = function () {
+        // only on first start
+        if (state.type === '') { updateState(); }
+
+        // test if counting down, if so, pause, otherwise start
+        if (typeof state.timeoutId == "number") {
+            window.clearTimeout(state.timeoutId);
+            state.timeoutId = undefined;
+        } else {
+            countdown();
+        }
+    };
+
+    $(document).ready(function () {
+        setTarget();
+
+        $('fieldset input').on('change', function (e) {          
+            setTarget();
+        }).on('click', function (e) {
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+        });
+
+        $('body').on('keypress', function (e) {
+            var code = e.keyCode || e.which;
+            // only continue if enter key is pressed
+            if (code === 13) {
+                toogleTimer();
+            }
+        });
+
+        $('#container').on('click', function (e) {
+            toogleTimer();
+        });
     });
-
-    breakTime.on('change', function(evt) {
-      if(countdownType === 0) {
-        target = breakTime.val() * 60;
-      }
-    });
-
-    rest.on('change', function(evt) {
-      if(countdownType === 0) {
-        target = rest.val() * 60;
-      }
-    });
-
-    runToggler.on('click', function(evt) {
-      if(typeof timeoutId == "number") {
-        window.clearTimeout(timeoutId);
-        timeoutId = undefined;
-        runToggler.text('Start');
-      } else {
-        ticker();
-        runToggler.text('Pause');
-      }
-    });
-
-    target = work.val() * 60;
-
-    $('body').on('keypress', function(e) {
-      var code = e.keyCode || e.which;
-      if(code === 13) {
-        runToggler.trigger('click');
-      }
-    });
-  });
 
 })(jQuery);
